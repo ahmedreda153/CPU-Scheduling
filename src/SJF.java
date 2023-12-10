7a467f75cc52a227dcc685bcab20e2b2359b40bb
Habiba A. Elbehairy <101597165+habibaelbehairy@users.noreply.github.com>
2023-12-10 16:46:35 +0200

    SJF Modify

--- src/SJF.java
+++ src/SJF.java
@@ -3,7 +3,7 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-class SJF {
+public class SJF {
     List<Process> processes = new ArrayList<>();
     public SJF(List<Process> processes) {
         this.processes = processes;
@@ -12,23 +12,21 @@ class SJF {
         Collections.sort(processes, Comparator.comparingInt(p -> p.burstTime));
         int time = 0;
         for (Process process : processes) {
+            process.setStartingTime(time);
             System.out.println("Process " + process.processID + " starts at " + time + " ends at " + (time + process.burstTime));
-            process.setStartingTime(time + process.burstTime);
             if (!processes.isEmpty()) {
                 System.out.println("Context Switching to next process at time " + time);
-                
             }
             time += process.burstTime;
         }
+        System.out.println(processes.get(0).startingTime);
         averageWaitingTime(processes);
     }
     public void averageWaitingTime(List<Process> processes){
         int totalWaitingTime = 0;
         for (Process process : processes) {
-            totalWaitingTime += process.startingTime - process.burstTime;
+            totalWaitingTime += process.startingTime;
         }
         System.out.println("Average Waiting Time: " + totalWaitingTime / processes.size());
     }
 }
-
-
