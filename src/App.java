a95b25dc82ee446a47625ea6b36d0716e4d10781
ahmedmo112 <amhk11827@gmail.com>
2023-12-14 16:52:53 +0200

    starvation srft Co-authored-by: RowanMadeeh <RowanMadeeh@users.noreply.github.com>

--- src/App.java
+++ src/App.java
@@ -1,9 +1,10 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class App {
       public static void main(String[] args) throws Exception {
         List<Process> processes = new ArrayList<>();
-        processes.add(new Process(1, 7,0));
+        processes.add(new Process(1, 8,0));
         processes.add(new Process(2, 4,2));
         processes.add(new Process(3, 1,4));
         processes.add(new Process(4, 4,5));
